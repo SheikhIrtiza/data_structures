826136a581fb400896fba4ad0f83641e2c657399
SheikhIrtiza <90492317+SheikhIrtiza@users.noreply.github.com>
2021-12-24 19:39:00 +0530

    Add files via upload

---
+++ array_sum.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int arr[] = {2, 4, 6, 8};
+    int size = sizeof arr/sizeof arr[0];
+    int total = 0;
+    printf("The sizeof arr %1u and the sizeof arr[0] % 1u \n",
+           sizeof arr, sizeof arr[0]);
+    for(int i = 0; i < size; i++){
+        total = total + arr[i];
+    }
+
+    printf("The sum o array = %d\n", total);
+    return 0;
+}
