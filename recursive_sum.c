45360ed992151d08a68633917560b44854406b22
Sheikh Irtiza <90492317+SheikhIrtiza@users.noreply.github.com>
2023-05-16 23:59:16 +0530

    Update recursive_sum.c

--- recursive_sum.c
+++ recursive_sum.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <assert.h>
-
+// function to use integer as argument of the function
 int sum_recursive(int num)
 {
   assert(num >= 0);
   if(num == 0)
     return num;
   else
+//     printing the value of the sum of numbers recursively by returning the sum of nmbers using return
     return (num+sum_recursive(num - 1));
 }
   int main(void)
