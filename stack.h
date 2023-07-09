5c9f5fce83cc53f8125571bb04551a1e25f602f7
Sheikh Irtiza <90492317+SheikhIrtiza@users.noreply.github.com>
2023-07-09 22:57:17 +0530

    Update stack.h

--- stack.h
+++ stack.h
@@ -13,7 +13,7 @@
 }Stack;
 
  void stack_init(Stack *stack);
- void stack_push(Stack *stack, StackEntry item);
+ void stack_push(Stack *stack, StackEntry item); 
  void stack_pop(Stack *stack, StackEntry* item);
  bool stack_is_empty(Stack *stack);
  bool stack_is_full(Stack *stack);
